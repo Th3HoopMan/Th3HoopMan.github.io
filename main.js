25486549734666de8bfe8685a609576d91b4608f
Joseph Hooper <stayvaliant94@gmail.com>
2016-11-24 01:46:20 -0500

    Designed the Title Page Added Icons folder Implemented bootstrap and sass Animated arrow Initial title page stylings

---
+++ main.js
@@ -0,0 +1,51 @@
+
+$(document).ready(function(){
+var flashingArrow;
+
+  $("#titleAbout").mouseenter(function(){
+
+    flashingArrow = setInterval(function(){
+    // toggle the class every five second
+    $("#titleArrow1").css("visibility", "visible");
+    setTimeout(function(){
+     // toggle back after 1 second
+     $("#titleArrow1").css("visibility", "hidden");
+   }, 350)
+ },700);
+  });
+
+  $("#titleAbout").mouseleave(function(){
+       clearInterval(flashingArrow);
+  });
+
+  $("#titleProjects").mouseenter(function(){
+    $("#titleArrow2").css("visibility", "visible");
+    flashingArrow = setInterval(function(){
+    // toggle the class every five second
+    $("#titleArrow2").css("visibility", "visible");
+    setTimeout(function(){
+     // toggle back after 1 second
+     $("#titleArrow2").css("visibility", "hidden");
+   }, 350)
+ }, 700);
+  });
+
+  $("#titleProjects").mouseleave(function(){
+       clearInterval(flashingArrow);
+  });
+
+  $("#titleContact").mouseenter(function(){
+    flashingArrow = setInterval(function(){
+    // toggle the class every five second
+    $("#titleArrow3").css("visibility", "visible");
+    setTimeout(function(){
+     // toggle back after 1 second
+     $("#titleArrow3").css("visibility", "hidden");
+   }, 350)
+ }, 700);
+  });
+
+  $("#titleContact").mouseleave(function(){
+       clearInterval(flashingArrow);
+  });
+});
